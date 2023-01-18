1534924086a609c0d621fa4040c7e4c5b66a9b4d
willzer0 <pembunuh4444@gmail.com>
2023-01-18 10:20:22 +0700

    penambahan login page front-end

--- OneDrive/Dokumen/test/test-1/src/App.js
+++ OneDrive/Dokumen/test/test-1/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Myarticle from "../src/pages/Myarticle";
+import Login from "./pages/Login";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Myarticle />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ OneDrive/Dokumen/test/test-1/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Login() {
+  return (
+    <body className="flex justify-center items-center w-[100%] h-[100vh]">
+      <div className="border-[2px] border-slate-700 w-[300px] h-[300px]">
+        hdahdhjajhdhjjsa
+      </div>
+    </body>
+  );
+}
