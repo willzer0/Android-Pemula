c152334f38ef82edbafd554805c7addd7b5f8059
willzer0 <pembunuh4444@gmail.com>
2023-01-19 09:33:24 +0700

    penambahan edit page

--- OneDrive/Dokumen/test/test-1/src/App.js
+++ OneDrive/Dokumen/test/test-1/src/App.js
@@ -4,11 +4,12 @@ import Myarticle from "../src/pages/Myarticle";
 import Login from "./pages/Login";
 import Edit from "./pages/EditComments";
 import EditComments from "./pages/EditComments";
+import Editpage from "./pages/Editpage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<EditComments />} />
+        <Route path="/" element={<Editpage />} />
       </Routes>
     </BrowserRouter>
   );
