e584a8133b8737c856e71c6304e96324b7d9d32f
willzer0 <pembunuh4444@gmail.com>
2023-01-19 07:30:48 +0700

    penambahan page edit

--- OneDrive/Dokumen/test/test-1/src/App.js
+++ OneDrive/Dokumen/test/test-1/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Myarticle from "../src/pages/Myarticle";
 import Login from "./pages/Login";
+import Edit from "./pages/EditComments";
+import EditComments from "./pages/EditComments";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<EditComments />} />
       </Routes>
     </BrowserRouter>
   );
